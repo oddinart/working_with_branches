d310e3ccb803685aa34fcf479f56842fe4bd829a
Avenberg <oddinartfj@gmail.com>
2024-03-25 22:58:20 +0500

    func_making_first_letter_bigger

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
+"""
+
+this function is doing big letters from small letters
+
+"""
 def big_letters(a):
     return a.upper()
+
+"""
+this function is making first letter of word big(?)
+"""
+def first_letter_big(a):
+    return a.title()
+
